7b4539b2a472d49afafbb1a5cdd0ee0b8fd89328
J K <jonathanwork@protonmail.com>
2017-06-06 15:10:43 +0100

    added the applications controller

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   resources :projects
   resources :users, only: [:new, :edit, :update, :create, :show, :destroy]
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :applications, only: [:new, :create, :show]
 end
