731d95e9cc48665f18dfbb7c6b8f0b3e0b6bd61d
James Hicks <hicks052@gmail.com>
2017-06-08 17:39:36 +0100

    styling done to navbar and some linking

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,4 +19,17 @@ class ApplicationController < ActionController::Base
   devise_parameter_sanitizer.permit(:account_update, keys: [:username])
   end
 
+  private
+
+  def after_sign_in_path_for(resource_or_scope)
+    dashboard_path
+  end
+
+  def require_login
+    unless current_user
+      redirect_to new_user_session_path
+    end
+  end
+
+
 end
