3115b6a94606ab92b808d5e4f39fd182efa0d710
J K <jonathanwork@protonmail.com>
2017-06-13 17:05:57 +0100

    none

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-stylestuff6
 ActiveRecord::Schema.define(version: 20170613132452) do
- 
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
